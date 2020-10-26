10c2c0e098e6724843bffe81a4fd37a5a4d84ddf
laura-ash <lauraeashmore@gmail.com>
2020-10-26 19:23:15 +0000

    Add preselected category when editing recipe

--- app.py
+++ app.py
@@ -122,9 +122,8 @@ def add_recipe():
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-
     categories = mongo.db.categories.find().sort("category_name",1)
-    return render_template("pages/edit_recipe.html", recipe=recipe)
+    return render_template("pages/edit_recipe.html", recipe=recipe, categories=categories)
 
 
 @app.route('/recipes/<recipe>', methods=["GET"])
