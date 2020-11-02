6e640c1e1a7b84c5d1451e4d128a61f650cd3082
laura-ash <lauraeashmore@gmail.com>
2020-11-02 17:10:23 +0000

    Removed test route

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -33,11 +34,6 @@ def get_recipes():
     recipes = list(mongo.db.recipes.find())
     return render_template("pages/recipes.html", recipes=recipes,title="Recipe main page")
 
-
-@app.route("/<bob>")
-def get_bob(bob):
-    recipes = mongo.db.recipes.find()
-    return render_template("pages/recipes.html", recipes=recipes,title="Recipe main page",heaven=bob) 
 
 
 @app.route("/register", methods=["GET","POST"])
